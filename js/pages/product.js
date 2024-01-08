4a5cdb0e8c17752443d03d5190eca443577edef5
AnnaKalis <annkalis@gmail.com>
2024-01-08 21:01:57 +0100

    loader and errordisplay

--- js/pages/product.js
+++ js/pages/product.js
@@ -1,32 +1,39 @@
+import { displayError } from "../displayerror.js";
+
 const queryString = document.location.search;
 const params = new URLSearchParams(queryString);
 const id = params.get("id");
+const productContainer = document.querySelector(".product-container");
 
-const productContainer = document.querySelector(".product-container")
+productContainer.innerHTML = `<div class="loader"></div>`;
 
 const url = "http://ca-cms.local/wp-json/wc/store/products/" + id;
 
 async function getProduct() {
-    try {
-      const response = await fetch(url);
-      const getResult = await response.json();
-      createProduct(getResult);
-    } catch (error) {
-      console.log("Error occured, can't fetch products!");
+  try {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error(`Network response was not ok (status: ${response.status})`);
     }
+
+    const getResult = await response.json();
+    createProduct(getResult);
+  } catch (error) {
+    productContainer.innerHTML = displayError("An error occured when uploading the product from the server!");
   }
+}
 
-  getProduct();
+getProduct();
 
-
-  function createProduct (details) {
-    productContainer.innerHTML += 
-    `<img src="${details.images[0].src}" alt="${details.images[0].alt}" />
+function createProduct(details) {
+    productContainer.innerHTML = "";
+  productContainer.innerHTML += `<img src="${details.images[0].src}" alt="${details.images[0].alt}" />
         <div class="main-product-info">
             <h1>${details.name}</h1>
-            <p>${details.prices.price/100} ${details.prices.currency_symbol}</p>
+            <p>${details.prices.price / 100} ${details.prices.currency_symbol}</p>
             <button class="cta">Add to cart</button>
         </div>
         <div class="description">${details.description}</div>
-    `
-  }
+    `;
+}
--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+import { displayError } from "./displayerror.js";
+
 const url = "http://ca-cms.local/wp-json/wc/store/products";
 const productsContainer = document.querySelector(".products");
 
@@ -6,27 +8,28 @@ productsContainer.innerHTML = `<div class="loader"></div>`;
 async function getProducts() {
   try {
     const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error(`Network response was not ok (status: ${response.status})`);
+    }
+
     const getResults = await response.json();
     createProducts(getResults);
   } catch (error) {
-    console.log("Error occured, can't fetch products!");
+    productsContainer.innerHTML = displayError("An error occured when uploading the products from the server!"
+    );
   }
 }
-
 
 getProducts();
 
 function createProducts(products) {
+  productsContainer.innerHTML = "";
   products.forEach(function (product) {
-    console.log(products)
-    productsContainer.innerHTML += 
-    `<a href="product.html?id=${product.id}" class="product-card">
+    productsContainer.innerHTML += `<a href="product.html?id=${product.id}" class="product-card">
         <h3>${product.name}</h3>
         <img src="${product.images[0].src}" alt="${product.images[0].alt}" />
-        <p>${product.prices.price/100} ${product.prices.currency_symbol}</p>
+        <p>${product.prices.price / 100} ${product.prices.currency_symbol}</p>
     </a>`;
   });
 }
-
-const queryString = document.location.search;
-const params = new URLSearchParams(queryString);
